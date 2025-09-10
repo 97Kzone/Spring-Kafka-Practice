d93ebbfbdf902b2ff8fa0175905697e967147e71
97Kzone <76652908+97Kzone@users.noreply.github.com>
2025-09-11 00:48:23 +0900

    FEAT. Comment CUD API 구현

---
+++ service/comment/src/main/java/kzone/board/comment/controller/CommentController.java
@@ -0,0 +1,31 @@
+package kzone.board.comment.controller;
+
+import kzone.board.comment.service.CommentService;
+import kzone.board.comment.service.request.CommentCreateRequest;
+import kzone.board.comment.service.response.CommentResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @GetMapping("/v1/comments/{commentId}")
+    public CommentResponse read(
+            @PathVariable("commentId") Long commentId
+    ) {
+        return commentService.read(commentId);
+    }
+
+    @PostMapping("/v1/comments")
+    public CommentResponse create(@RequestBody CommentCreateRequest request) {
+        return commentService.create(request);
+    }
+
+    @DeleteMapping("/v1/comment/{commentId}")
+    public void delete(
+            @PathVariable("commentId") Long commentId) {
+        commentService.delete(commentId);
+    }
+}
