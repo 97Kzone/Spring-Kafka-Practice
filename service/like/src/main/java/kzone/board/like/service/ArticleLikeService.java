d6827c260a6653d53505d7c4818f7143b4ac3e28
97Kzone <76652908+97Kzone@users.noreply.github.com>
2025-09-16 23:45:27 +0900

    FEAT. 좋아요 V1 기능, 테스트 구현

---
+++ service/like/src/main/java/kzone/board/like/service/ArticleLikeService.java
@@ -0,0 +1,40 @@
+package kzone.board.like.service;
+
+import jakarta.transaction.Transactional;
+import kzone.board.common.snowflake.Snowflake;
+import kzone.board.like.entity.ArticleLike;
+import kzone.board.like.repository.ArticleLikeRepository;
+import kzone.board.like.service.response.ArticleLikeResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleLikeService {
+    private final Snowflake snowflake = new Snowflake();
+    private final ArticleLikeRepository articleLikeRepository;
+
+    public ArticleLikeResponse read(Long articleId, Long userId) {
+        return articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .map(ArticleLikeResponse::from)
+                .orElseThrow();
+    }
+
+    @Transactional
+    public void like(Long articleId, Long userId) {
+        articleLikeRepository.save(
+                ArticleLike.create(
+                        snowflake.nextId(),
+                        articleId,
+                        userId
+                )
+        );
+    }
+
+    @Transactional
+    public void unlike(Long articleId, Long userId) {
+        articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .ifPresent(articleLikeRepository::delete);
+    }
+}
