7eb73fb45be3bb97853bae3502c0e20ad9f70a4e
97Kzone <76652908+97Kzone@users.noreply.github.com>
2025-09-20 14:16:25 +0900

    FEAT. Redis base 조회수 어뷰징 방지 추가

--- service/view/src/test/java/kzone/board/view/api/ViewApiTest.java
+++ service/view/src/test/java/kzone/board/view/api/ViewApiTest.java
@@ -18,7 +18,7 @@ public class ViewApiTest {
         for (int i = 0; i < 10000; i++) {
             executorService.execute(() -> {
                 restClient.post()
-                        .uri("/v1/articles-views/articles/{articleId}/users/{usersId}", 3L, 1L)
+                        .uri("/v1/articles-views/articles/{articleId}/users/{usersId}", 4L, 1L)
                         .retrieve();
                 latch.countDown();
             });
@@ -27,7 +27,7 @@ public class ViewApiTest {
         latch.await();
 
         Long count = restClient.get()
-                .uri("/v1/articles-views/articles/{articleId}/count", 3L)
+                .uri("/v1/articles-views/articles/{articleId}/count", 4L)
                 .retrieve()
                 .body(Long.class);
 
