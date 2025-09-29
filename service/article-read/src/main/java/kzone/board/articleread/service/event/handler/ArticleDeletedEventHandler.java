b18473b742a4a5dc848de8a46ee42a76a7bb8b99
97Kzone <76652908+97Kzone@users.noreply.github.com>
2025-09-29 23:19:27 +0900

    FEAT. 게시글 목록 최적화 Service & Controller 추가

--- service/article-read/src/main/java/kzone/board/articleread/service/event/handler/ArticleDeletedEventHandler.java
+++ service/article-read/src/main/java/kzone/board/articleread/service/event/handler/ArticleDeletedEventHandler.java
@@ -1,6 +1,8 @@
 package kzone.board.articleread.service.event.handler;
 
+import kzone.board.articleread.repository.ArticleIdListRepository;
 import kzone.board.articleread.repository.ArticleQueryModelRepository;
+import kzone.board.articleread.repository.BoardArticleCountRepository;
 import kzone.board.common.event.Event;
 import kzone.board.common.event.EventType;
 import kzone.board.common.event.payload.ArticleDeletedEventPayload;
@@ -11,12 +13,16 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class ArticleDeletedEventHandler implements EventHandler<ArticleDeletedEventPayload> {
+    private final ArticleIdListRepository articleIdListRepository;
     private final ArticleQueryModelRepository articleQueryModelRepository;
+    private final BoardArticleCountRepository boardArticleCountRepository;
 
     @Override
     public void handle(Event<ArticleDeletedEventPayload> event) {
         ArticleDeletedEventPayload payload = event.getPayload();
+        articleIdListRepository.delete(payload.getBoardId(),  payload.getArticleId());
         articleQueryModelRepository.delete(payload.getArticleId());
+        boardArticleCountRepository.createOrUpdate(payload.getBoardId(),  payload.getBoardArticleCount());
     }
 
     @Override
